9f0667eec42c7725ef2dcde9a182645c972a0a09
NielMc <niel@codeinstitute.net>
2021-11-02 10:39:11 +0000

    delete calc code

--- 3_calculator.py
+++ 3_calculator.py
@@ -1,12 +1,1 @@
-import streamlit as st
-from app_pages.multi_page import MultiPage
 
-from app_pages.page_calculator import calculator_body
-
-app = MultiPage(app_name="Calculator App")  # Create an instance of the app
-
-
-# Add your app pages here using .add_page()
-app.app_page("Calculator", calculator_body)
-
-app.run()
