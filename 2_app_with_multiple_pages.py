55d15c6d74cb5013d71cb114cc9c47a826fc55d2
NielMc <niel@codeinstitute.net>
2021-11-02 10:38:42 +0000

    Remove multipage code

--- 2_app_with_multiple_pages.py
+++ 2_app_with_multiple_pages.py
@@ -1,14 +1,1 @@
-import streamlit as st
-from app_pages.multi_page import MultiPage
 
-from app_pages.page1 import page1_body
-from app_pages.page2 import page2_body
-
-# Create an instance
-app = MultiPage(app_name="This is my first multi-page Streamlit App")
-
-# Add your app pages here using .add_page()
-app.app_page("Page 1", page1_body)
-app.app_page("Page 2", page2_body)
-
-app.run()
